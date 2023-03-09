f75e752fef1899180f6ba7db66f7ab830b725947
RafaelMatos <rafael-matos@outlook.com>
2023-03-09 17:54:53 -0300

    feat(confirm): section confitm order created

---
+++ src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
@@ -0,0 +1,27 @@
+import { Button } from '../../../../components/Button'
+import { RegularText } from '../../../../components/Typography'
+import { ConfirmationSectionContainer } from './styles'
+
+export function ConfirmationSection() {
+  return (
+    <ConfirmationSectionContainer>
+      <div>
+        <RegularText size="s">Total de itens</RegularText>
+        <RegularText>R$29,70</RegularText>
+      </div>
+      <div>
+        <RegularText size="s">Entrega</RegularText>
+        <RegularText>R$3,50</RegularText>
+      </div>
+      <div>
+        <RegularText size="l" color="subtitle" weight={700}>
+          Total
+        </RegularText>
+        <RegularText size="l" color="subtitle" weight={700}>
+          R$33,20
+        </RegularText>
+      </div>
+      <Button text="CONFIRMAR PEDIDO" />
+    </ConfirmationSectionContainer>
+  )
+}
--- src/pages/CompleteOrder/components/SelectedCoffees/styles.ts
+++ src/pages/CompleteOrder/components/SelectedCoffees/styles.ts
@@ -13,3 +13,24 @@ export const DetailsContainer = styled(SectionBaseStyle)`
   display: flex;
   flex-direction: column;
 `
+export const ConfirmationSectionContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  button {
+    background: ${({ theme }) => theme.colors['brand-yellow']};
+    border: none;
+    border-radius: 6px;
+    padding: 1rem 0;
+    transition: 0.5s;
+
+    &:hover {
+      background: ${({ theme }) => theme.colors['brand-yellow-dark']};
+    }
+  }
+`
