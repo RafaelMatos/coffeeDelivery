cd20b5c304c7f4a4907ef5292c2e650417d107d7
RafaelMatos <rafael-matos@outlook.com>
2023-03-13 15:36:27 -0300

    feat(confirmed):confirmed page styles done

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
 import { HomePage } from './pages/Home'
-import { CartPage } from './pages/Cart'
 import { CompleteOrderPage } from './pages/CompleteOrder'
+import { OrderConfirmedPage } from './pages/OrderConfirmed'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/cart" element={<CartPage />}></Route>
         <Route path="/completeOrder" element={<CompleteOrderPage />}></Route>
+        <Route path="/orderConfirmed" element={<OrderConfirmedPage />}></Route>
       </Route>
     </Routes>
   )
--- src/pages/Cart/index.tsx
+++
@@ -1,3 +0,0 @@
-export function CartPage() {
-  return <div>CartPage</div>
-}
